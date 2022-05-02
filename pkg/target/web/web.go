bfa6fcf37df31db5b2e26c5748e007ec0853b7ab
Grace Punzalan <gracepunzalan@graces-mbp.lan>
2022-05-02 12:27:16 -0400

    retry logic

--- pkg/target/web/web.go
+++ pkg/target/web/web.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"time"
 
@@ -13,8 +12,9 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const runWebPageTestUrl = "https://www.webpagetest.org/runtest.php"
-const checkWebPageTestUrl = "https://www.webpagetest.org/jsonResult.php"
+const RUN_WEB_PAGE_TEST_URL = "https://www.webpagetest.org/runtest.php"
+const CHECK_WEB_PAGE_TEST_URL = "https://www.webpagetest.org/jsonResult.php"
+const MAX_RETRY = 3
 
 type Web struct {
 	tests  map[string][]config.Test
@@ -30,7 +30,7 @@ func (w *Web) Init(c *config.Config) error {
 	return nil
 }
 
-// InitParams sets additiona; configuration to run web performance testing.
+// InitParams sets additional configuration to run web performance testing.
 func (w *Web) InitParams(targetParams target.TargetParams) error {
 	w.http = targetParams.HttpClient
 	w.apiKey = targetParams.ApiKeys["web"]
@@ -43,19 +43,20 @@ func (w *Web) Test(appLabel string) error {
 	appTests := w.tests[appLabel]
 
 	testIds := []string{}
+	// Run web performance tests.
 	for _, test := range appTests {
 		testId := w.runTest(appLabel, test)
 		testIds = append(testIds, testId)
 		logrus.Printf("[performance][%s] Ran webpagetest: url=%s | testId=%s", appLabel, test.Url, testId)
 	}
 
+	// Check the status for each test given a testId.
 	for _, testId := range testIds {
 		statusCh := make(chan struct{})
 		go w.checkStatus(statusCh, appLabel, testId)
 
 		go func(testId string) {
 			<-statusCh
-
 			output, err := json.Marshal(statusCh)
 			if err != nil {
 				logrus.Errorf("[performance][%s] Error marshaling status check: testId=%s | error=%s", appLabel, testId, err)
@@ -68,54 +69,61 @@ func (w *Web) Test(appLabel string) error {
 }
 
 func (w *Web) runTest(appLabel string, test config.Test) string {
-	url := fmt.Sprintf("%s?k=%s&url=%s&mobile=%d&runs=%d&f=json", runWebPageTestUrl, w.apiKey, test.Url, test.Mobile, test.Runs)
-	resp, err := w.http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		logrus.Errorf("[performance][%s] Error reading body from webpagetest result: url=%s | error=%s", appLabel, test.Url, err)
-	}
-
-	// TODO Need to validate the actual reponse body.
 	var responseData struct {
 		data struct {
 			testId string
 		}
 	}
-	json.Unmarshal(body, &responseData)
-
+	retryCount := 0
+	url := fmt.Sprintf("%s?k=%s&url=%s&mobile=%d&runs=%d&f=json", RUN_WEB_PAGE_TEST_URL, w.apiKey, test.Url, test.Mobile, test.Runs)
+	for retryCount <= MAX_RETRY {
+		resp, err := w.http.Get(url)
+		if err != nil {
+			retryCount++
+			logrus.Errorf("[performance][%s] Error running webpagetest: url=%s | retryCount=%d | error=%s", appLabel, test.Url, retryCount, err)
+			time.Sleep(time.Duration(retryCount) * time.Second)
+		} else {
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				logrus.Errorf("[performance][%s] Error reading webpagetest result: url=%s | error=%s", appLabel, test.Url, err)
+			}
+			json.Unmarshal(body, &responseData)
+			break
+		}
+    }
 	return responseData.data.testId
 }
 
 func (w *Web) checkStatus(statusCh chan struct{}, appLabel string, testId string) {
-	for {
-		url := fmt.Sprintf("%s?test=%s", checkWebPageTestUrl, testId)
+	var responseData struct {
+		data       struct{}
+		statusCode int
+	}
+	retryCount := 0
+	url := fmt.Sprintf("%s?test=%s", CHECK_WEB_PAGE_TEST_URL, testId)
+	for retryCount <= MAX_RETRY {
 		resp, err := w.http.Get(url)
 		if err != nil {
-			log.Fatal(err)
-		}
+			retryCount++
+			logrus.Errorf("[performance][%s] Error checking webpagetest: testId=%s | error=%s", appLabel, testId, err)
+			time.Sleep(time.Duration(retryCount) * time.Second)
+		} else {
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				logrus.Errorf("[performance][%s] Error reading webpagetest result: testId=%s | error=%s", appLabel, testId, err)
+			}
+			json.Unmarshal(body, &responseData)
 
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			logrus.Errorf("[performance][%s] Error reading body from webpagetest result: testId=%s | error=%s", appLabel, testId, err)
+			status := responseData.statusCode 
+			switch {
+			case status == 200:
+				statusCh <- responseData.data
+				return
+			case status == 102 || status == 400 || status == 401:
+				retryCount++
+				time.Sleep(time.Duration(retryCount) * time.Second)
+				break
+			}
 		}
-
-		var responseData struct {
-			data       struct{}
-			statusCode int
-		}
-		json.Unmarshal(body, &responseData)
-
-		// TODO: Should we make a distinction between failed and successful test runs?
-		switch {
-		case responseData.statusCode >= 200:
-			statusCh <- responseData.data
-			return
-		}
-
-		time.Sleep(3000 * time.Second)
-	}
+    }
 }
