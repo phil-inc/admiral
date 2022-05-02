d93be20ea20880dc52abdacd2fbe261e1d295819
Grace Punzalan <gracepunzalan@graces-mbp.lan>
2022-05-02 17:45:56 -0400

    refactor api calls

--- pkg/target/web/web.go
+++ pkg/target/web/web.go
@@ -22,6 +22,22 @@ type Web struct {
 	apiKey string
 }
 
+type QueueResponse struct {
+	data struct {
+		testId string
+	}
+	statusCode int
+}
+
+type ResultsResponse struct {
+	data 	   TestResult
+	statusCode int
+}
+
+type TestResult struct {
+	id string
+}
+
 // Init creates the configuration for web performance testing
 func (w *Web) Init(c *config.Config) error {
 	tests := c.Performance.Target.Web.Tests
@@ -42,87 +58,153 @@ func (w *Web) InitParams(targetParams target.TargetParams) error {
 func (w *Web) Test(appLabel string) error {
 	appTests := w.tests[appLabel]
 
-	testIds := []string{}
 	// Run web performance tests.
+	testIds := []string{}
 	for _, test := range appTests {
-		testId := w.runTest(appLabel, test)
+		testId, err := w.runTestWithRetries(appLabel, test) 
+		if err != nil {
+			logrus.Errorf("[performance][%s] Error running test | testUrl =%s | error=%s ", appLabel, test.Url, err)
+			continue;
+		}
 		testIds = append(testIds, testId)
-		logrus.Printf("[performance][%s] Ran webpagetest: url=%s | testId=%s", appLabel, test.Url, testId)
 	}
 
-	// Check the status for each test given a testId.
+	// Get result for each testId.
 	for _, testId := range testIds {
-		statusCh := make(chan struct{})
-		go w.checkStatus(statusCh, appLabel, testId)
+		resultsCh := make(chan TestResult) 
+		go w.getResultsWithRetries(resultsCh, appLabel, testId) 
 
 		go func(testId string) {
-			<-statusCh
-			output, err := json.Marshal(statusCh)
+			<-resultsCh
+			output, err := json.Marshal(resultsCh)
 			if err != nil {
-				logrus.Errorf("[performance][%s] Error marshaling status check: testId=%s | error=%s", appLabel, testId, err)
+				logrus.Errorf("[performance][%s] Error marshaling test result: testId=%s | error=%s", appLabel, testId, err)
 			}
-			logrus.Printf("[performance][%s] received webpagetest result: testId=%s | result=%s", appLabel, testId, string(output))
+			logrus.Printf("[performance][%s] Successfully received test result: testId=%s | result=%s", appLabel, testId, string(output))
 		}(testId)
 	}
 
 	return nil
 }
 
-func (w *Web) runTest(appLabel string, test config.Test) string {
-	var responseData struct {
-		data struct {
-			testId string
+func (w *Web) runTestWithRetries(appLabel string, test config.Test) (string, error) {
+	var testId string
+	var err error
+	retry := 0
+	for retry <= MAX_RETRY {
+		resp, err := w.queueTest(appLabel, test)
+		if err != nil {
+			retry++
+			logrus.Errorf("[performance][%s] Error queueing test | retry=%d | error=%s", appLabel, retry, err)
+			time.Sleep(time.Duration(retry) * time.Second)
+		}
+		
+		respObj := w.convertQueueResponse(appLabel, resp)
+		testId, err = w.handleQueueResponse(appLabel, respObj)
+		if err != nil {
+			retry++
+			logrus.Errorf("[performance][%s] Error handling queue response | retry=%d | error=%s", appLabel, retry, err)
+			time.Sleep(time.Duration(retry) * time.Second)
 		}
 	}
-	retryCount := 0
-	url := fmt.Sprintf("%s?k=%s&url=%s&mobile=%d&runs=%d&f=json", RUN_WEB_PAGE_TEST_URL, w.apiKey, test.Url, test.Mobile, test.Runs)
-	for retryCount <= MAX_RETRY {
-		resp, err := w.http.Get(url)
-		if err != nil {
-			retryCount++
-			logrus.Errorf("[performance][%s] Error running webpagetest: url=%s | retryCount=%d | error=%s", appLabel, test.Url, retryCount, err)
-			time.Sleep(time.Duration(retryCount) * time.Second)
-		} else {
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				logrus.Errorf("[performance][%s] Error reading webpagetest result: url=%s | error=%s", appLabel, test.Url, err)
-			}
-			json.Unmarshal(body, &responseData)
-			break
-		}
-    }
-	return responseData.data.testId
+	return testId, err 
 }
 
-func (w *Web) checkStatus(statusCh chan struct{}, appLabel string, testId string) {
-	var responseData struct {
-		data       struct{}
-		statusCode int
-	}
-	retryCount := 0
-	url := fmt.Sprintf("%s?test=%s", CHECK_WEB_PAGE_TEST_URL, testId)
-	for retryCount <= MAX_RETRY {
-		resp, err := w.http.Get(url)
-		if err != nil {
-			retryCount++
-			logrus.Errorf("[performance][%s] Error checking webpagetest: testId=%s | error=%s", appLabel, testId, err)
-			time.Sleep(time.Duration(retryCount) * time.Second)
-		} else {
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				logrus.Errorf("[performance][%s] Error reading webpagetest result: testId=%s | error=%s", appLabel, testId, err)
-			}
-			json.Unmarshal(body, &responseData)
+func (w *Web) queueTest(appLabel string, test config.Test) (*http.Response, error) {
+	var resp *http.Response
+	var err error
+	url := fmt.Sprintf("%s?k=%s&url=%s&mobile=%d&runs=%d&f=json", RUN_WEB_PAGE_TEST_URL, w.apiKey, test.Url, test.Mobile, test.Runs)
 
-			switch responseData.statusCode {
-			case 200:
-				statusCh <- responseData.data
-				return
-			case 102, 400, 401:
-				retryCount++
-				time.Sleep(time.Duration(retryCount) * time.Second)
-			}
+	resp, err =  w.http.Get(url)
+	if err != nil {
+		logrus.Errorf("[performance][%s] Error queuing webpagetest: url=%s | error=%s", appLabel, url, err)
+	}
+
+	return resp, err
+}
+
+func (w *Web) convertQueueResponse(appLabel string, resp *http.Response) QueueResponse {
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Errorf("[performance][%s] Error converting to queue response | error=%s", appLabel, err)
+	}
+	var respObj QueueResponse
+	json.Unmarshal(body, &respObj)
+
+	return respObj
+}
+
+func (w *Web) handleQueueResponse(appLabel string, respObj QueueResponse) (string, error){
+	var testId string
+	var err error
+	switch respObj.statusCode {
+	case 200:
+		testId = respObj.data.testId
+		logrus.Printf("[performance][%s] Successfully queued test: | testId=%s", appLabel, testId)
+	default:
+		err = fmt.Errorf("[performance][%s] Failed to get test ID:  statusCode=%d", appLabel, respObj.statusCode)
+	}
+	return testId, err
+}
+
+func (w *Web) getResultsWithRetries(resultsCh chan TestResult, appLabel string, testId string) {
+	var results TestResult
+	retry := 0
+	for retry <= MAX_RETRY {
+		resp, err := w.checkStatus(appLabel, testId) 
+		if err != nil {
+			retry++
+			logrus.Errorf("[performance][%s] Error queueing test | retry=%d | error=%s", appLabel, retry, err)
+			time.Sleep(time.Duration(retry) * time.Second)
 		}
+
+		respObj := w.convertToResultsResponse(appLabel, resp)
+		results, err = w.handleResultsResponse(appLabel, respObj)
+
+		if err != nil {
+			retry++
+			logrus.Errorf("[performance][%s] Error handling queue response | retry=%d | error=%s", appLabel, retry, err)
+			time.Sleep(time.Duration(retry) * time.Second)
+		}
+
     }
-	statusCh <- responseData.data
+	resultsCh <- results
+}
+
+func (w *Web) checkStatus(appLabel string, testId string) (*http.Response, error) {
+	var resp *http.Response
+	var err error
+	url := fmt.Sprintf("%s?test=%s", CHECK_WEB_PAGE_TEST_URL, testId)
+	resp, err =  w.http.Get(url)
+	if err != nil {
+		logrus.Errorf("[performance][%s] Error queuing webpagetest: url=%s | error=%s", appLabel, url, err)
+	}
+
+	return resp, err
+}
+
+func (w *Web) convertToResultsResponse(appLabel string, resp *http.Response) ResultsResponse {
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Errorf("[performance][%s] Error converting to results response | error=%s", appLabel, err)
+	}
+	var respObj ResultsResponse
+	json.Unmarshal(body, &respObj)
+
+	return respObj
+}
+
+func (w *Web) handleResultsResponse(appLabel string, respObj ResultsResponse) (TestResult, error){
+	var result TestResult
+	var err error
+	switch respObj.statusCode {
+	case 100, 101:
+	case 200:
+		result = respObj.data
+		logrus.Printf("[performance][%s] Successfully received test result: | testId=%s", appLabel, result.id)
+	default:
+		err = fmt.Errorf("[performance][%s] Failed to get test result:  statusCode=%d", appLabel, respObj.statusCode)
+		
+	}
+	return result, err
 }
