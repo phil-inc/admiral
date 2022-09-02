32cbe16976f9dfe1107366e13b5d9f01d357d01f
Mitchell Dyer <mitchelldyer01@gmail.com>
2022-09-02 18:45:10 +0000

    Remove random logger

--- pkg/logstores/loki/loki.go
+++ pkg/logstores/loki/loki.go
@@ -40,7 +40,6 @@ func (l *Loki) Init(c *config.Config) error {
 // Stream sends the logs to Loki
 func (l *Loki) Stream(entry chan utils.LogEntry) {
 	for e := range entry {
-		logrus.Println(e)
 		if e.Err == nil {
 			go l.Send(e.Text, e.Metadata)
 		}
